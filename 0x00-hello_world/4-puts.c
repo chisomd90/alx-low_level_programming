6b4045260a4bba184bf74a1f316f7cea93b7a860
chisom <chisomd90@gmail.com>
2022-12-08 16:42:52 -0800

    task 2

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * return: Always 0 (sucess)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
