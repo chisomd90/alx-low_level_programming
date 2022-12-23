d7d08af51cee8e0755a545a1ef595cc527aa4fad
chisom <chisomd90@gmail.com>
2022-12-22 19:24:34 -0800

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -16,8 +16,6 @@ char *cap_string(char *str)
 		if (str[i - 1] == ' ' ||
 		    str[i - 1] == '\t' ||
 		    str[i - 1] == '\n' ||
-		    str[i - 1] == '{' ||
-		    str[i - 1] == '}' ||
 		    str[i - 1] == ',' ||
 		    str[i - 1] == ';' ||
 		    str[i - 1] == '.' ||
@@ -26,6 +24,8 @@ char *cap_string(char *str)
 		    str[i - 1] == '"' ||
 		    str[i - 1] == '(' ||
 		    str[i - 1] == ')' ||
+		    str[i - 1] == '{' ||
+		    str[i - 1] == '}' ||
 		    i == 0)
 			str[i] -= 32;
 		i++;
