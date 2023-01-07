13a35a7115a9ba640d36f2d9d93674f7dfd8eb31
chisom <chisomd90@gmail.com>
2023-01-06 19:40:03 -0800

    tasks

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+* main - Entry point
+*
+* puts - prints a string
+*
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
