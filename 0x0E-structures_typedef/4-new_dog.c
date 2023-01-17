0aa1704da5667b925afcbc2a6e76198c2931133a
chisom <chisomd90@gmail.com>
2023-01-17 14:55:55 -0800

    task 3

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -31,6 +31,7 @@ int _strlen(char *s)
 char *_strcpy(char *dest, char *src)
 {
 	int len, i;
+
 	len = 0;
 	while (src[len] != '\0')
 	{
@@ -55,6 +56,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
 	int len1, len2;
+
 	len1 = _strlen(name);
 	len2 = _strlen(owner);
 	dog = malloc(sizeof(dog_t));
