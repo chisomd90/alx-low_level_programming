e184bfb52f48aeb2e17e15f5a7ec33658119eb43
chisom <chisomd90@gmail.com>
2023-01-18 19:48:34 -0800

    task 4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
 	arr = (char *)main;
 	for (i = 0; i < bytes; i++)
 	{
-		if (i == bytes -1)
+		if (i == bytes - 1)
 		{
 			printf("%02hhx\n", arr[i]);
 			break;
