424ab6621847f89f51e6013bacaab906a3ff0530
chisomd90 <chisomd90@gmail.com>
2023-02-07 12:33:12 -0800

    tasks

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -6,8 +6,8 @@
  * @text_content: The string to add to the end of the file.
  *
  * Return: If the function fails or filename is NULL - -1.
- * If the file does not exist the user lacks write permissions - -1.
- * Otherwise - 1.
+ *        If the file does not exist the user lacks write permissions - -1.
+ *              Otherwise - 1.
  */
 
 int append_text_to_file(const char *filename, char *text_content)
@@ -17,7 +17,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	return (-1);
+	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
 			len++;
